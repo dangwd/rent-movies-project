e99813b11f635ce5060a53821601a71283707b6e
Cheems <nhokcodon282002@gmail.com>
2024-12-07 11:30:18 +0700

    update layout, login

--- src/views/client/layouts/AppLayout.vue
+++ src/views/client/layouts/AppLayout.vue
@@ -6,14 +6,29 @@ import AppSidebarRight from './AppSidebarRight.vue';
     <body class="font-montserrat text-sm bg-white dark:bg-zinc-900">
         <div class="flex min-h-screen 2xl:max-w-screen-2xl 2xl:mx-auto 2xl:border-x-2 2xl:border-gray-200 dark:2xl:border-zinc-700">
             <!-- Left Sidebar -->
-            <AppSidebarLeft></AppSidebarLeft>
+            <aside class="w-1/6 py-10 px-5 min-w-min border-r border-gray-300 dark:border-zinc-700 hidden md:block">
+                <AppSidebarLeft></AppSidebarLeft>
+            </aside>
             <!-- /Left Sidebar -->
-            <main class="flex-1 py-10 px-5 sm:px-10">
+            <ScrollPanel
+                style="height: 100vh"
+                :pt="{
+                    bary: 'hidden'
+                }"
+                class="flex-1 py-10 px-5 sm:px-10"
+            >
                 <router-view></router-view>
-            </main>
-
+            </ScrollPanel>
             <!-- Right Sidebar -->
-            <AppSidebarRight></AppSidebarRight>
+            <ScrollPanel
+                style="height: 100vh"
+                class="w-1/5 p-10 min-w-min border-l border-gray-300 dark:border-zinc-700 hidden lg:block"
+                :pt="{
+                    bary: 'hidden'
+                }"
+            >
+                <AppSidebarRight></AppSidebarRight>
+            </ScrollPanel>
             <!-- /Right Sidebar -->
         </div>
     </body>
